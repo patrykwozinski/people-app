d926d3d8e8efd4513a32715421aa1bdf699c6ec7
Patryk Woziński <patryk.wozinski@gmail.com>
2020-03-14 22:13:47 +0100

    mylib <3

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule People.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:calendar_workdays, "~> 0.1.0"}
     ]
   end
 
